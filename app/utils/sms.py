4d8ff7275a17e1f21209c4018c6d8b2b5fc3b689
kimathikim <briankimathi94@gmail.com>
2024-08-25 00:24:56 +0300

    afs

--- app/utils/sms.py
+++ app/utils/sms.py
@@ -7,48 +7,29 @@ load_dotenv()
 
 # Initialize Africa's Talking
 
+api_key = os.getenv("AFSAPI")
 uname = "ImmunSys"
-api_key = "91a098b314aeda715976967be01d9127f00d619fedd5157385977c4185d4061e"
 africastalking.initialize(
-    username="ImmunSys",
+    username=uname,
     api_key=os.getenv("AFSAPI"),
 )
-
-
-# class SendSMS:
-#     def sending(self):
-#         recipients = ["+254796699970", "+254787353102"]
-#         message = "Hello mavin. Welcome to immunization tracking system.\nWe value the health of your Child!"
-#         sender = 796699970
-#         print(sender, recipients, message)
-#         try:
-#             sms = africastalking.SMS
-#             print(sms)
-#             response = sms.send(
-#                 message,
-#                 recipients,
-#             )
-#             print(response)
-#         except Exception as e:
-#             print(f"Houston, we have a problem: {e}")
-#
-#
-# sms_instance = SendSMS()
-# sms_instance.sending()
 
 
 def send_code_to_patient(phone_number, patient_code):
     """This function sends a patient code via SMS."""
     try:
         message = f"Hello, your patient code is {patient_code}."
-        recipients = [f"+254{phone_number}"]
+        recipients = [f"+255{phone_number}"]
         africastalking.initialize(uname, api_key)
         sender = 796699970
         print(sender, recipients, message)
         try:
             sms = africastalking.SMS
             if sms is not None:
-                response = sms.send(message, recipients)
+                response = sms.send(
+                    message,
+                    recipients,
+                )
                 print(response)
         except Exception as e:
             print(f"Mother father, we have a problem: {e}")
